27cd36ef6026c3bf8d6b585cd1eaa80be8c1c87b
Esdras de la Torre <esdras.business.02@gmail.com>
2025-02-03 17:04:32 -0600

    feat: Array class first implementation

--- src/main/java/arrays/onedimension/OneDimensionArrayMain.java
+++ src/main/java/arrays/onedimension/OneDimensionArrayMain.java
@@ -1,7 +1,14 @@
 package arrays.onedimension;
 
+import java.util.ArrayList;
+
 public class OneDimensionArrayMain {
     public static void main(String[] args) {
-
+        var array = new ArrayList<Integer>();
+        array.add(2);
+        array.add(3);
+        System.out.println(array.toString());
+        Object object = "3";
+        System.out.println((String) object + "3");
     }
 }
--- src/main/java/arrays/onedimension/utils/Array.java
+++ src/main/java/arrays/onedimension/utils/Array.java
@@ -1,5 +1,79 @@
 package arrays.onedimension.utils;
 
-public class Array{
+import java.util.Arrays;
+
+public class Array<T>{
+
+     private Object[] array;
+     private final int INITIAL_SIZE = 1;
+     private int length = 0;
+     private int size = INITIAL_SIZE;
+
+     public Array(){
+         this.array = new Object[INITIAL_SIZE];
+     }
+
+     public Array(int initialSize){
+         this.array = new Object[initialSize];
+     }
+
+     public void add(T element){
+         if(this.size == this.length){
+             expandCapacity();
+         }
+         this.array[length++] = element;
+     }
+
+     //TODO Implement this method
+     public void addAt(int index){
+
+     }
+
+    //TODO Implement this method
+     public void remove(int index){
+
+     }
+
+
+    //TODO Implement this method
+     private void expandCapacity(){
+         this.size = this.size * 2;
+         this.array = Arrays.copyOf(this.array, this.size);
+     }
+
+    //TODO Implement this method
+    public int getLength() {
+        return length;
+    }
+
+    //TODO Implement this method
+    public T[] toArray() {
+        return (T[]) array;
+    }
+
+    //TODO Implement this method
+    public T get(int index){
+         return null;
+    }
+
+    //TODO Implement this method
+    public boolean contains(T element){
+         return false;
+    }
+
+    //TODO Implement this method
+    public boolean isEmpty(){
+         return false;
+    }
+
+    //TODO Implement this method
+    public void clear(){
+
+    }
+
+    //TODO Implement this method
+    public String toString(){
+         return null;
+    }
 
 }
