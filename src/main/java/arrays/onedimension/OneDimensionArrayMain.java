27dc133187f846d77b816a185cd3bf16b8843b85
Esdras de la Torre <esdras.business.02@gmail.com>
2025-02-03 18:23:31 -0600

    feat: practices added

--- src/main/java/arrays/onedimension/OneDimensionArrayMain.java
+++ src/main/java/arrays/onedimension/OneDimensionArrayMain.java
@@ -24,9 +24,32 @@ public class OneDimensionArrayMain {
         System.out.println("Practice 1");
         System.out.println(ingeniverseArray.get(0)); //prints 3
         System.out.println("Practice 2");
-        System.out.println(ingeniverseArray.get(-1));// Prints invalid index
-        System.out.println(ingeniverseArray.get(5));// Prints invalid index
-
+        try{
+//            System.out.println(ingeniverseArray.get(-1));// Prints invalid index
+//            System.out.println(ingeniverseArray.get(5));// Prints invalid index
+            System.out.println(ingeniverseArray.get(3));//Prints invalid index
+        }catch (Exception exception){
+            System.out.println("NOOO mi compa se pasó");
+            System.out.println(exception.getMessage());
+        }
+        System.out.println("Practice 3");
+        System.out.println(ingeniverseArray.contains(5)); //true
+        System.out.println(ingeniverseArray.contains(10));//false
+        System.out.println("Practice 4");
+        var ingeniverseArray2 = new Array<String>();
+        System.out.println(ingeniverseArray2.isEmpty());//Prints true
+        ingeniverseArray2.add("Hello");
+        System.out.println(ingeniverseArray2.isEmpty());//Prints false
+        System.out.println("Practice 5");
+        var ingeniverseArray3 = new Array<String>();
+        System.out.println(ingeniverseArray3.isEmpty());// Prints true
+        ingeniverseArray3.add("HELLO");
+        System.out.println(ingeniverseArray3.isEmpty());// Prints false
+        ingeniverseArray3.clear();
+        System.out.println(ingeniverseArray3.isEmpty());// Prints true
+        System.out.println("Practice 6");
+        System.out.println(ingeniverseArray2.toString());
+        System.out.println(ingeniverseArray3.toString());
 
     }
 }
--- src/main/java/arrays/onedimension/utils/Array.java
+++ src/main/java/arrays/onedimension/utils/Array.java
@@ -2,44 +2,42 @@ package arrays.onedimension.utils;
 
 import java.util.Arrays;
 
-public class Array<T>{
+public class Array<T> {
 
-     private Object[] array;
-     private final int INITIAL_SIZE = 1;
-     private int length = 0;
-     private int size = INITIAL_SIZE;
+    private Object[] array;
+    private final int INITIAL_SIZE = 1;
+    private int length = 0;
+    private int size = INITIAL_SIZE;
 
-     public Array(){
-         this.array = new Object[INITIAL_SIZE];
-     }
+    public Array() {
+        this.array = new Object[INITIAL_SIZE];
+    }
 
-     public Array(int initialSize){
-         this.array = new Object[initialSize];
-     }
+    public Array(int initialSize) {
+        this.array = new Object[initialSize];
+    }
 
-     public void add(T element){
-         if(this.size == this.length){
-             expandCapacity();
-         }
-         this.array[length++] = element;
-     }
-
-     //TODO Implement this method
-     public void addAt(int index){
-
-     }
+    public void add(T element) {
+        if (this.size == this.length) {
+            expandCapacity();
+        }
+        this.array[length++] = element;
+    }
 
     //TODO Implement this method
-     public void remove(int index){
+    public void addAt(int index) {
 
-     }
-
+    }
 
     //TODO Implement this method
-     private void expandCapacity(){
-         this.size = this.size * 2;
-         this.array = Arrays.copyOf(this.array, this.size);
-     }
+    public void remove(int index) {
+
+    }
+
+    private void expandCapacity() {
+        this.size = this.size * 2;
+        this.array = Arrays.copyOf(this.array, this.size);
+    }
 
     public int getLength() {
         return length;
@@ -49,34 +47,51 @@ public class Array<T>{
         return size;
     }
 
-    //TODO Implement this method
     public T[] toArray() {
         return (T[]) array;
     }
 
-    //TODO Implement this method
-    public T get(int index){
-         return (T) this.array[index];
+    public T get(int index) {
+        if (index < 0 || index > length || array[index] == null) {
+            throw new ArrayIndexOutOfBoundsException("Invalid index");
+        }
+        return (T) this.array[index];
+    }
+
+    public boolean contains(T element) {
+        //Ciclo for
+        for (int i = 0; i < this.length; i++) {
+            if (this.array[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    public void clear() {
+        this.array = new Object[this.INITIAL_SIZE];
+        this.length = 0;
+        this.size = this.INITIAL_SIZE;
     }
 
     //TODO Implement this method
-    public boolean contains(T element){
-         return false;
-    }
-
-    //TODO Implement this method
-    public boolean isEmpty(){
-         return false;
-    }
-
-    //TODO Implement this method
-    public void clear(){
-
-    }
-
-    //TODO Implement this method
-    public String toString(){
-         return null;
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (this.isEmpty()) {
+            sb.append("Empty");
+        } else {
+            sb.append("[");
+            for (Object element : this.array) {
+                sb.append(element.toString());
+                sb.append(" ");
+            }
+            sb.append("]");
+        }
+        return sb.toString();
     }
 
 }
